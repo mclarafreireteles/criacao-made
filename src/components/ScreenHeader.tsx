43d33a9fdad18f982f87bd6d336a7c8200865796
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-08-18 22:23:27 -0300

    style: adiciona componente de header

---
+++ src/components/ScreenHeader.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import Colors from '@/constants/Colors';
+
+type Props = {
+  title: string;
+  rightAccessory?: React.ReactNode;
+};
+
+export function ScreenHeader({ title, rightAccessory }: Props) {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.sideContainer}>
+        {router.canGoBack() && (
+          <Pressable onPress={() => router.back()} hitSlop={20}>
+            <Ionicons name="arrow-back" size={24} color={Colors.light.text} />
+          </Pressable>
+        )}
+      </View>
+      
+      <Text style={styles.title}>{title}</Text>
+      
+      {/* Acessório da Direita (logo, etc.) */}
+      <View style={styles.sideContainer}>{rightAccessory}</View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingTop: 40,
+  },
+  sideContainer: {
+    width: 50,
+    alignItems: 'flex-start',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
