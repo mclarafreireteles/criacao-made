ac0c34f1df71ec2e92f7afae1a64798456ac909d
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-09-24 22:46:01 -0300

    fix: layout da aplicacao

---
+++ src/app/(panel)/edit_game/page.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function EditGameScreen(){
+    return(
+        <View>
+            <Text>Em construção</Text>
+        </View>
+    )
+}
