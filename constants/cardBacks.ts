cc4506f1ad50e9d07e4aab6dace1365b2842c737
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-10-13 15:09:03 -0300

    add: pasta constants

---
+++ constants/cardBacks.ts
@@ -0,0 +1,26 @@
+export const cardBacks = [
+    {
+        id: 'sports',
+        image: require('@/assets/images/card_backs/sports.png'),
+    },
+    {
+        id: 'languages',
+        image: require('@/assets/images/card_backs/languages.png'),
+    },
+    {
+        id: 'math_one',
+        image: require('@/assets/images/card_backs/math_one.png'),
+    },
+    {
+        id: 'math_two',
+        image: require('@/assets/images/card_backs/math_two.png'),
+    },
+    {
+        id: 'nature',
+        image: require('@/assets/images/card_backs/nature.png'),
+    },
+    {
+        id: 'humanities',
+        image: require('@/assets/images/card_backs/humanities.png'),
+    },
+];
